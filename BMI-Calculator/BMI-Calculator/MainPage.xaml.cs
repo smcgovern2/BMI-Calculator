23e110510dc777ec92540d20fbbbf30d29727eed
smcgovern2 <smcgovern2@my.wctc.edu>
2021-11-05 10:37:56 -0500

    Add project files.

---
+++ BMI-Calculator/BMI-Calculator/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace BMI_Calculator
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        void Btn_Clicked(object sender, System.EventArgs e)
+        {
+            Double weight = Int32.Parse(Inp_weight.Text);
+            Double height = Int32.Parse(Inp_height.Text);
+            Double bmi = (weight / height / height) * 703.00;
+
+            Lbl_BMI.Text = $"Your BMI is {bmi.ToString("F2")}.";
+        }
+    }
+}
